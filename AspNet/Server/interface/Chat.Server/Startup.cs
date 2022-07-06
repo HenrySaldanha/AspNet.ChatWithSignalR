7d933304dc6a0b4783f7c069f5c3c148b4c124cc
Henry Lucas Saldanha <henry.l.saldanha@gmail.com>
2022-07-05 21:43:22 -0300

    Add server config

---
+++ AspNet/Server/interface/Chat.Server/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Application;
+
+namespace Chat.Server;
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddSignalR();
+        services.AddCors();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+        app.UseRouting();
+
+        app.UseCors(p =>
+        {
+            p.WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+        });
+
+        app.UseEndpoints(endpoints => { endpoints.MapHub<ChatHub>("/chat"); });
+    }
+}
